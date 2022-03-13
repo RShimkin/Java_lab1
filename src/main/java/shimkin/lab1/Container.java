62dd5a723fd4dd718185905af559f97b150afb26
RShimkin <roma.shimkin@yandex.ru>
2022-03-13 18:11:56 +0300

    initial commit

---
+++ src/main/java/shimkin/lab1/Container.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package shimkin.lab1;
+
+import java.lang.reflect.Array;
+
+/**
+ *
+ * @author User
+ */
+public class Container<T> {
+    
+}
